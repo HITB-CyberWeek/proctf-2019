145578c49d41ec7c835efc9bf7b7848bbb777e82
Alexander Bersenev <bay@hackerdom.ru>
2019-06-26 03:05:17 +0500

    fraud_detector: consume less memory in peak

--- services/fraud_detector/start.sh
+++ services/fraud_detector/start.sh
@@ -7,4 +7,4 @@ chown root:root /home/fraud_detector
 chmod 755 /home/fraud_detector /home/fraud_detector/static /home/fraud_detector/data /home/fraud_detector/data/users
 chmod 700 /home/fraud_detector/data/rules
 
-exec gunicorn -w 16 app:application -b 0.0.0.0:10000 -u fraud_detector -g fraud_detector --capture-output --timeout=10 --reload
+exec gunicorn -w 8 app:application -b 0.0.0.0:10000 -u fraud_detector -g fraud_detector --capture-output --timeout=4 --reload
