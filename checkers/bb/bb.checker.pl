22df8a36f29984d7831963f38bef687ec5369ef0
Andrey Khozov <avkhozov@gmail.com>
2019-08-03 11:45:12 +0500

    [bb] init checker

---
+++ checkers/bb/bb.checker.pl
@@ -0,0 +1,246 @@
+#!/usr/bin/perl
+
+use Mojo::Base -strict;
+
+use Mojo::Collection 'c';
+use Mojo::Log;
+use Mojo::UserAgent;
+use Mojo::Util 'trim';
+
+no warnings 'experimental::smartmatch';
+
+my ($SERVICE_OK, $SERVICE_CORRUPT, $SERVICE_MUMBLE, $SERVICE_FAIL, $INTERNAL_ERROR) =
+  (101, 102, 103, 104, 110);
+my %MODES = (check => \&check, get => \&get, put => \&put);
+my ($mode, $ip) = splice @ARGV, 0, 2;
+my @chars = ('A' .. 'Z', 'a' .. 'z', '_', '0' .. '9');
+
+warn 'Invalid input. Empty mode or ip address.' and exit $INTERNAL_ERROR unless defined $mode and defined $ip;
+warn 'Invalid mode.' and exit $INTERNAL_ERROR unless $mode ~~ %MODES;
+
+my $agents = c(<DATA>)->map(sub {chomp; $_});
+my $ua = _ua();
+
+my $url = Mojo::URL->new("http://$ip:8080/");
+my $log = Mojo::Log->new;
+$log->info("URL for check: $url");
+
+exit $MODES{$mode}->(@ARGV);
+
+sub _corrupt { say $_[0] and $log->info($_[0]) and exit $SERVICE_CORRUPT }
+sub _mumble  { say $_[0] and $log->info($_[0]) and exit $SERVICE_MUMBLE }
+sub _fail    { say $_[0] and $log->info($_[0]) and exit $SERVICE_FAIL }
+
+sub _ua {
+  my $ua = Mojo::UserAgent->new(max_redirects => 3);
+  $ua->transactor->name($agents->shuffle->first);
+
+  return $ua;
+}
+
+sub _check_http_response {
+  my $tx = shift;
+
+  if (my $err = $tx->error) {
+    if ($err->{code}) {
+      _mumble "$err->{code} response: $err->{message}";
+    } else {
+      _fail "Connection error: $err->{message}";
+    }
+  } else {
+    return $tx->result;
+  }
+}
+
+sub check {
+  $log->info('Getting public advertisements ...');
+  $url->path('/');
+  $_ = _check_http_response($ua->get($url));
+  my $title = $_->dom->find('h2')->map('text')->first // '';
+  _mumble "Invalid main page" unless $title =~ /List of all advertisements/;
+
+  $log->info('Trying to get board');
+  $url->path('/messages/board/100');
+  $_ = _check_http_response(my $tx = $ua->get($url));
+  _mumble "Invalid redirect for board" unless my $prev = $tx->previous;
+  $_ = _check_http_response($prev);
+  _mumble "Invalid response for board" unless $_->code == 302;
+
+  # paging
+
+  my $login = '';
+  $login .= $chars[rand @chars] for 1 .. 20;
+  $log->info("Login by invalid user [login:$login] ...");
+  $url->path('/login');
+  $_ = _check_http_response($ua->post($url => form => {login => $login, password => $login}));
+  my $error = $_->dom->find('blockquote')->map('text')->first // '';
+  _mumble "Invalid login process" unless $error =~ /user not found/;
+
+  return $SERVICE_OK;
+}
+
+sub get {
+  my ($id, $flag) = @_;
+
+  my ($login1, $login2, $password) = split ':', $id;
+
+  $log->info("Login by user1 [login:$login1] ...");
+  $url->path('/login');
+  $_ = _check_http_response($ua->post($url => form => {login => $login1, password => $password}));
+
+  my $links = $_->dom->find('nav a')->map('text')->join('\n');
+  _mumble "Can't login by $login1" unless $links =~ qr/$login1/;
+
+  $log->info('Check ad ...');
+  $url->path_query('/?own=1');
+  $_ = _check_http_response($ua->get($url));
+  my $link = $_->dom->find('a.thread')->first;
+  my $link_text = trim $link->all_text;
+  $log->info("Got link [link:$link_text]");
+  _mumble "Invalid thread link" unless $link_text =~ qr/$login2/;
+
+  my $href = $link->attr('href');
+  $log->info("Go to thread [href:$href]");
+  $url->path_query($href);
+  $_ = _check_http_response($ua->get($url));
+  my $page_message = $_->dom->find('.message.s8 span')->map('text')->first;
+  $log->info("Got message [message:$page_message]");
+
+  _corrupt "Invalid flag" unless $flag eq $page_message;
+
+  return $SERVICE_OK;
+}
+
+sub put {
+  my ($id, $flag) = @_;
+
+  my $password = $id;
+  my ($login1, $login2) = ('', '');
+  $login1 .= $chars[rand @chars] for 1 .. 5;
+  $login2 .= $chars[rand @chars] for 1 .. 5;
+  my ($title, $descr) = ('', '');
+  $title .= $chars[rand @chars] for 1 .. 12;
+  $descr .= $chars[rand @chars] for 1 .. 50;
+
+  $log->info("Creating user1 [login:$login1] ...");
+  $url->path('/register');
+  $_ = _check_http_response($ua->post($url => form => {login => $login1, password => $password}));
+
+  $log->info("Creating ad [title:$title] ...");
+  $url->path('/ad/create');
+  $_ = _check_http_response($ua->post($url => form => {title => $title, descr => $descr}));
+  $_->dom->find('p.board_id')->map('text')->first =~ /ID: (\d+)/;
+  my $board_id = $1;
+
+  $log->info('Check ad ...');
+  $url->path_query('/?own=1');
+  $_ = _check_http_response($ua->get($url));
+  my $page_title = $_->dom->find('h5.board_title')->map('text')->join(' ');
+  my $page_desrc = $_->dom->find('p.board_descr')->map('text')->join(' ');
+  $log->info("Got ad [title:$page_title] [descr:$page_desrc]");
+  _mumble "Invalid own ad" unless $page_title =~ qr/$title/;
+  _mumble "Invalid own ad" unless $page_desrc =~ qr /$descr/;
+
+  $log->info("Creating user2 [login:$login2] ...");
+  $url->path('/register');
+  my $ua2 = _ua();
+  $_ = _check_http_response($ua2->post($url => form => {login => $login2, password => $password}));
+
+  $log->info("Creating message [board_id:$board_id] ...");
+  $url->path("/messages/board/$board_id");
+  $_ = _check_http_response($ua2->post($url => form => {message => $flag}));
+  my $page_message = $_->dom->find('.message.s8 span')->map('text')->first;
+  $log->info("Got message [message:$page_message]");
+  _mumble "Invalid message" unless $flag eq $page_message;
+
+  say "$login1:$login2:$id";
+  return $SERVICE_OK;
+}
+
+__DATA__
+Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko
+Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50
+Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36
+Mozilla/5.0 (Windows NT 6.3; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Windows NT 6.1; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36
+Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36
+Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36
+Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.8 (KHTML, like Gecko) Version/9.1.3 Safari/601.7.8
+Mozilla/5.0 (iPad; CPU OS 10_0_2 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A456 Safari/602.1
+Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36
+Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0;  Trident/5.0)
+Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;  Trident/5.0)
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14
+Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko
+Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0
+Mozilla/5.0 (Windows NT 5.1; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586
+Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko
+Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36
+Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0
+Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/53.0.2785.109 Mobile/14A456 Safari/601.1.46
+Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_2 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A456 Safari/602.1
+Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17
+Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0
+Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36
+Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/52.0.2743.116 Chrome/52.0.2743.116 Safari/537.36
+Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36
+Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17
+Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36
+Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36
