a43e796496aed746f96d67a7272b439bdf4e1b2b
Andrey Khozov <avkhozov@gmail.com>
2019-09-03 23:32:34 +0500

    [cs] add deer to test game

--- checkers/rubik/checker.sh
+++ checkers/rubik/checker.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-cd `dirname $0`/src >/dev/null 2>&1
-exec dotnet run -c Release "$@"
+cd `dirname $0`/src/bin/Release/netcoreapp2.2/publish >/dev/null 2>&1
+exec dotnet rubikchecker.dll "$@"
