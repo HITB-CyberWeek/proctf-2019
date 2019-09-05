a93f971acbc880238169b20bbd01faee96152406
Andrey Khozov <avkhozov@gmail.com>
2019-09-05 22:47:45 +0500

    [setpton] fix checker.sh

--- checkers/SePtoN/checker.sh
+++ checkers/SePtoN/checker.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-cd `dirname $0`/src/bin/Release/netcoreapp2.2/publish >/dev/null 2>&1
+cd `dirname $0`/SePtoN_Checker/bin/Release/netcoreapp2.2/publish >/dev/null 2>&1
 exec dotnet SePtoN_Checker.dll "$@"
