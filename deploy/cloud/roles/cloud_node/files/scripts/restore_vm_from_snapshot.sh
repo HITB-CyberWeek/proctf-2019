39da0c53d33fe7d76d3af434ebe3622a11d97260
Alexander Bersenev <bay@hackerdom.ru>
2019-09-24 04:36:33 +0500

    cloud: restore snapshots for multiple vms

--- deploy/cloud/roles/cloud_node/files/scripts/restore_vm_from_snapshot.sh
+++ deploy/cloud/roles/cloud_node/files/scripts/restore_vm_from_snapshot.sh
@@ -1,14 +1,26 @@
 #!/bin/bash -e
 
-TEAM=${1?Syntax: ./restore_vm_from_snapshot.sh <team_id> <name>}
-NAME=${2?Syntax: ./restore_vm_from_snapshot.sh <team_id> <name>}
+TEAM=${1?Syntax: ./restore_vm_from_snapshot.sh <team_id> <vm_num> <vm_name> <name>}
+VMNUM=${2?Syntax: ./restore_vm_from_snapshot.sh <team_id> <vm_num> <vm_name> <name>}
+VMNAME=${3?Syntax: ./restore_vm_from_snapshot.sh <team_id> <vm_num> <vm_name> <name>}
+NAME=${4?Syntax: ./restore_vm_from_snapshot.sh <team_id> <vm_num> <vm_name> <name>}
 
 if ! [[ $TEAM =~ ^[0-9]+$ ]]; then
   echo "team number validation error"
   exit 1
 fi
 
-vm="test_team${TEAM}"
+if ! [[ $VMNUM =~ ^[0-9]+$ ]]; then
+  echo "vm number validation error"
+  exit 1
+fi
+
+if ! [[ $VMNAME =~ ^[0-9a-zA-Z_]+$ ]]; then
+  echo "vm name validation error"
+  exit 1
+fi
+
+vm="${VMNUM}_${VMNAME}_team${TEAM}"
 
 # check if snapshot exists
 if ! VBoxManage snapshot "$vm" showvminfo "$NAME" &>/dev/null; then
@@ -28,4 +40,4 @@ MY_NAME="`readlink -f "$0"`"
 MY_DIR="`dirname "$MY_NAME"`"
 cd "${MY_DIR}"
 
-./launch_vm.sh "$TEAM"
+./launch_vm.sh "$TEAM" "$VMNUM" "$VMNAME"
