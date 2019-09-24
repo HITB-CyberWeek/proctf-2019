3f059c6bb3dc7386c420d57920fa67d9a8acf908
Alexander Bersenev <bay@hackerdom.ru>
2019-09-24 05:09:12 +0500

    cloud: better vm attaching

--- deploy/cloud/roles/cloud_node/files/scripts/attach_vm_gui.sh
+++ deploy/cloud/roles/cloud_node/files/scripts/attach_vm_gui.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-TEAM=${1?Syntax: ./attach_vm_gui.sh <team_id> [fix]}
-FIX=${2}
+TEAM=${1?Syntax: ./attach_vm_gui.sh <team_id> <vm_num> <vm_name> [fix]}
+VMNUM=${2?Syntax: ./attach_vm_gui.sh <team_id> <vm_num> <vm_name> [fix]}
+VMNAME=${3?Syntax: ./attach_vm_gui.sh <team_id> <vm_num> <vm_name> [fix]}
+FIX=${4}
 
 if ! [[ $TEAM =~ ^[0-9]+$ ]]; then
   echo "team number validation error"
   exit 1
 fi
 
-vm="test_team${TEAM}"
+if ! [[ $VMNUM =~ ^[0-9]+$ ]]; then
+  echo "vm number validation error"
+  exit 1
+fi
+
+if ! [[ $VMNAME =~ ^[0-9a-zA-Z_]+$ ]]; then
+  echo "vm name validation error"
+  exit 1
+fi
+
+vm="${VMNUM}_${VMNAME}_team${TEAM}"
 
 # fix keyboard layout
 if [ "$FIX" == fix ]; then
@@ -16,4 +28,4 @@ if [ "$FIX" == fix ]; then
  setxkbmap us -print | xkbcomp - $DISPLAY
 fi
 
-VirtualBox --startvm "$vm" --separate
+VirtualBoxVM --startvm "$vm" --separate
