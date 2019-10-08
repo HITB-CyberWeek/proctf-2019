348a49d9be06fc976bc3740c74eed581c32a3a46
Konstantin Plotnikov <kost@hackerdom.ru>
2019-10-09 02:59:08 +0500

    gallery service startup fix

--- services/gallery/start.sh
+++ services/gallery/start.sh
@@ -5,4 +5,4 @@ export PYTHONUNBUFFERED=1
 chown -R gallery:gallery /home/gallery
 chmod -R 700 /home/gallery
 
-exec gunicorn -w 3 app:application -b 0.0.0.0:80 -u gallery -g gallery --capture-output --timeout=15 --reload
+exec gunicorn -w 2 app:application -b 0.0.0.0:80 -u gallery -g gallery --capture-output --timeout=60 --reload
