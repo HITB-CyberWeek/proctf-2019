77ae1ef50947b1563ad82239e377aba668bcae5f
dimmo <dimmoborgir@gmail.com>
2019-10-09 00:43:54 +0500

    [tracker] better app start in docker. tune log level

--- services/tracker/main.py
+++ services/tracker/main.py
@@ -1,25 +1,74 @@
 #!/usr/bin/env python3
-
+import argparse
 import asyncio
+import logging
+import time
 
-from app.common import configure_logging
+import asyncpg
+
+from app.common import configure_logging, connect_db
 from app.config import get_config
 from app.network import create_server_socket
 from app.protocol import handle_client, register_handlers
 
 
-async def run_server():
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--drop", action="store_true")
+    return parser.parse_args()
+
+
+async def create_tables(db):
+    with open("db/create.sql") as f:
+        sql = f.read()
+    try:
+        await db.execute(sql)
+        logging.info("Empty DB found. New tables have been created.")
+    except asyncpg.exceptions.DuplicateTableError:
+        pass
+
+
+async def drop_tables(conn):
+    logging.warning("Dropping database tables ...")
+    with open("db/drop.sql") as f:
+        sql = f.read()
+    await conn.execute(sql)
+    logging.warning("All tables have been dropped.")
+
+
+async def async_main(loop, config, args):
+    db = await wait_for_db_connection()
+    if args.drop:
+        await drop_tables(db)
+        return
+    await create_tables(db)
+    await db.close()
+
+    register_handlers()
+    server = create_server_socket(config.host, config.port)
     while True:
         client_sock, _ = await loop.sock_accept(server)
         loop.create_task(handle_client(client_sock, loop))
 
 
-config = get_config()
+async def wait_for_db_connection():
+    db_retry_time_sec = 5
+    while True:
+        try:
+            return await connect_db()
+        except Exception as e:
+            logging.error("DB not available: %s. Retrying in %d seconds ...", e, db_retry_time_sec)
+            time.sleep(db_retry_time_sec)
 
-configure_logging(config.debug)
-register_handlers()
 
-server = create_server_socket(config.host, config.port)
+def main():
+    args = parse_args()
+    config = get_config()
+    configure_logging(config.debug)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run_server())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(async_main(loop, config, args))
+
+
+if __name__ == "__main__":
+    main()
