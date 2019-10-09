7a0fd1899f275da1a7de2d66f454fd6b6fbb6698
Alexander Bersenev <bay@hackerdom.ru>
2019-10-10 00:50:24 +0500

    startup: shorter checksums

--- services/startup/src/hashsum/src/lib.rs
+++ services/startup/src/hashsum/src/lib.rs
@@ -9,7 +9,7 @@ pub fn calc_hash(data: &[u8]) -> String {
         q = hex::encode(sha2::Sha256::digest((q + &c.to_string()).as_bytes()))
     }
 
-    return q.into();
+    return q[..32].into();
 }
 
 #[cfg(test)]
@@ -18,8 +18,8 @@ mod tests {
 
     #[test]
     fn hash_good() {
-        assert_eq!(calc_hash(&[]), "5c1419ac91dac01faef6f3e4c59ac776788a31f7c34da0b750a769161745bc0b");
-        assert_eq!(calc_hash(&[1]), "4bc7656e710322bd6b2d92ca80aac42c897b2b33b6d4f7b6b07cfd85e3326c48");
-        assert_eq!(calc_hash(b"test"), "dd1b11c50f4fedd2c29cff6a947601753e9791fcd8c8d186f742c8a5e489fdf1");
+        assert_eq!(calc_hash(&[]), "5c1419ac91dac01faef6f3e4c59ac776");
+        assert_eq!(calc_hash(&[1]), "4bc7656e710322bd6b2d92ca80aac42c");
+        assert_eq!(calc_hash(b"test"), "dd1b11c50f4fedd2c29cff6a94760175");
     }
 }
