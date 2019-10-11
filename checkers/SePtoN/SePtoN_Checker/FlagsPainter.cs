aa4407f6f603ca2aa6ff8d2cc1a5a066e4f5cd6e
Konstantin Plotnikov <kost@hackerdom.ru>
2019-10-11 07:11:49 +0500

    SePtoN checker now generates random image filling before painting a flag

--- checkers/SePtoN/SePtoN_Checker/FlagsPainter.cs
+++ checkers/SePtoN/SePtoN_Checker/FlagsPainter.cs
@@ -13,11 +13,20 @@ namespace SePtoN_Checker
 		public static byte[] DrawFlag(string flag)
 		{
 			var image = new Bitmap(400, 40, PixelFormat.Format32bppArgb);
+			var r = new Random();
+			for(int x = 0; x < image.Width; x++)
+			{
+				for(int y = 0; y < image.Height; y++)
+				{
+					image.SetPixel(x, y, Color.FromArgb(255, r.Next(256), r.Next(256), r.Next(256)));
+				}
+			}
+
 			var rectangle = new RectangleF(0, 0, image.Width, image.Height);
 			using(Graphics g = Graphics.FromImage(image))
 			{
 				g.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
-				g.FillRectangle(new SolidBrush(Color.Red), 0, 0, image.Width, image.Height);
+//				g.FillRectangle(new SolidBrush(Color.Red), 0, 0, image.Width, image.Height);
 				StringFormat sf = new StringFormat();
 				sf.Alignment = StringAlignment.Center;
 				sf.LineAlignment = StringAlignment.Center;
