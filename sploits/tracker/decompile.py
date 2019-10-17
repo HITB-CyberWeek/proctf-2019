180c1073f811a63a80e742ff0f6dba3b9e652736
simonov-d <simonov-d@yandex-team.ru>
2019-10-17 14:21:34 +0500

    [tracker] decompile.py rename variable

--- sploits/tracker/decompile.py
+++ sploits/tracker/decompile.py
@@ -5,6 +5,6 @@ import sys
 
 with open(sys.argv[1], "rb") as f:
     raw = f.read()
-    co2 = marshal.loads(raw)
-    uncompyle6.code_deparse(co2)
+    co = marshal.loads(raw)
+    uncompyle6.code_deparse(co)
     print()
