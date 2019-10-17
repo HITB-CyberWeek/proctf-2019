678e8d95330ebbdf35b79a06cea71054b9705c7b
simonov-d <simonov-d@yandex-team.ru>
2019-10-17 14:19:56 +0500

    [tracker] byte-code decompilation script

---
+++ sploits/tracker/decompile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import marshal
+import uncompyle6
+import sys
+
+with open(sys.argv[1], "rb") as f:
+    raw = f.read()
+    co2 = marshal.loads(raw)
+    uncompyle6.code_deparse(co2)
+    print()
